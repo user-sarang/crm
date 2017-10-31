3e4feaf340d4b7d1edd414b568f69001aa0d67d8
sarang <sarang05me43@gmail.com>
2017-10-30 22:17:42 -0500

    added usersarang.python.anywhere.com in allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'diz*jyb+_6g_sq$rhgx%3hu7+dk0oo7vr++)w!4)f@^$&eyh$$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['usersarang.pythonanywhere.com']
 
 
 # Application definition
